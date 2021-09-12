8151e6e5a15008ebe55e0c987bdef5200bf64051
River <ruchuanlin@gmail.com>
2021-09-12 16:50:34 +0800

    remove unnessary files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { fetchAllRecords } from "./actions/recordAction";
-import { fetchFocusRecord } from "./actions/focusRecordAction";
-import { fetchAllUsers } from "./actions/userAction";
-
-import DataTable from "./containers/DataTable";
-import Panel from "./containers/Panel";
-
-import TestWizard from './containers/TestWizard'
-
 const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchFocusRecord());
-    dispatch(fetchAllRecords());
-    dispatch(fetchAllUsers());
-  }, [dispatch]);
-
   return (
     <>
-      <Panel></Panel>
-      <TestWizard></TestWizard>
-      <DataTable></DataTable>
+      <h1>hi</h1>
     </>
   );
 };
