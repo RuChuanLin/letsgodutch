fdcadc5eba124c4752e21654567224ffcb4144ab
River <ruchuanlin@gmail.com>
2021-09-17 07:28:27 +0800

    replace with step wizard

--- src/components/StepWizard/index.js
+++ src/components/StepWizard/index.js
@@ -1,61 +1,66 @@
-import React, { useState, useRef } from "react";
-
-import { Typography, Carousel } from "antd";
-
+import React, { useState } from "react";
+import { Typography } from "antd";
+import StepWizard from "react-step-wizard";
 import WizardPanel from "./WizardPanel";
+import "./transitions.css";
 
-const StepWizard = ({
-  focusRecord,
-  setFocusRecord,
-  stepPages,
-  ExtraPanelInfo,
-}) => {
-  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+const transitions = {
+  enterRight: `animated enterRight`,
+  enterLeft: `animated enterLeft`,
+  exitRight: `animated exitRight`,
+  exitLeft: `animated exitLeft`,
+  intro: `animated intro`,
+};
 
-  const getPageInfo = () => stepPages[currentStepIndex];
+const Wizard = ({ focusRecord, setFocusRecord, stepPages, ExtraPanelInfo }) => {
+  const [state, setState] = useState({
+    activeStep: 0,
+  });
 
-  const slider = useRef();
+  console.log(focusRecord);
+  const setInstance = (SW) =>
+    setState({
+      ...state,
+      SW,
+    });
 
-  const validate = () => {
-    // const { errorMsgs } = focusRecord;
-    // if (!errorMsgs) {
-    //   return true;
-    // }
-    // return Object.entries(errorMsgs)
-    //   .filter(([_, existError]) => existError)
-    //   .map(([errorMsg]) => errorMsg);
-    return true; // TODO 之後研究怎麼寫驗證
+  const { SW, activeStep } = state;
+
+  const onStepChange = ({ activeStep }) => {
+    setState({ ...state, activeStep });
   };
 
   return (
     <div>
-      <Typography.Title level={3}>{getPageInfo()?.title}</Typography.Title>
-      <Carousel
-        lazyLoad="ondemand"
-        beforeChange={(_, i) => setCurrentStepIndex(i)}
-        dots={false}
-        infinite={false}
-        ref={(ref) => (slider.current = ref)}
+      <StepWizard
+        onStepChange={onStepChange}
+        transitions={transitions}
+        instance={setInstance}
       >
         {stepPages.map((stepPage, i) => {
           const step = `step${i}`;
           return (
-            <stepPage.Page
-              key={step}
-              focusRecord={focusRecord}
-              setFocusRecord={setFocusRecord}
-            ></stepPage.Page>
+            <div key={step}>
+              <Typography.Title level={3}>{stepPage?.title}</Typography.Title>
+              <stepPage.Page
+                focusRecord={focusRecord}
+                setFocusRecord={setFocusRecord}
+              ></stepPage.Page>
+            </div>
           );
         })}
-      </Carousel>
-      <WizardPanel
-        ExtraPanelInfo={ExtraPanelInfo}
-        errorMsgs={validate()}
-        currentStepIndex={currentStepIndex}
-        totalSteps={stepPages.length}
-        getStepControl={() => slider.current}
-      ></WizardPanel>
+      </StepWizard>
+      {SW && (
+        <WizardPanel
+          SW={SW}
+          activeStep={activeStep}
+          ExtraPanelInfo={ExtraPanelInfo}
+          errorMsgs={true}
+          totalSteps={stepPages.length}
+        ></WizardPanel>
+      )}
     </div>
   );
 };
-export default StepWizard;
+
+export default Wizard;
